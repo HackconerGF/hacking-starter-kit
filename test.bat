62ef4956ac0bb21b44a14521bcbc6435242f5916
HackconerGF <138873864+HackconerGF@users.noreply.github.com>
2023-07-15 12:42:50 +0300

    review what virus and reverse.bat does

---
+++ test.bat
@@ -0,0 +1,6 @@
+@echo off
+echo this is a test file
+pause
+echo nw at the virus.bat file it showes the administator user on login reverse.bat hides it
+pause
+echo for now virus and reverse its done
